9315f6365c95d2bd0dab644f26516b33a9a25e5a
julian <julian@the-lair.info>
2023-01-28 21:37:52 +0000

    chore: correct style errors

--- src/main/java/com/groupunix/Main.java
+++ src/main/java/com/groupunix/Main.java
@@ -1,10 +1,20 @@
 package com.groupunix;
 
 import com.groupunix.drivewireserver.DriveWireServer;
-import org.apache.commons.configuration.ConfigurationException;
 
-public class Main {
-  public static void main(String[] args) throws ConfigurationException {
+public final class Main {
+  /**
+   * Default entry point.
+   *
+   * @param args command line arguments
+   */
+  public static void main(final String[] args) {
     DriveWireServer.main(args);
+  }
+
+  /**
+   * Hidden default constructor.
+   */
+  private Main() {
   }
 }
